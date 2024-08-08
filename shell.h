6f1ddbb9337b31849234932af96479e22e605582
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-08-08 09:15:01 -0500

    tried to build one entire file instead of seperate files but i couldnt get it running for my life so this is the attempt to break it up into seperate functional files and maybe then it will compile and run

--- shell.h
+++ shell.h
@@ -3,18 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-#define BUFSIZE 1024
-#define MAX_ARGS 32
-
-void tokenize_input(char *buffer, char *args[]);
-int execute_command(char *args[], int num_args);
-int main(int argc, char **argv, char **env);
-void sigint_handler(int signum);
-void sigtstp_handler(int signum);
+void execmd(char **argv);
+char *get_location(char *command);
+char *_getenv(const char *name);
+char *_strdup(const char *str);
+void exec_command(char **argv, char *actual_command, int mallocd);
 
 #endif
